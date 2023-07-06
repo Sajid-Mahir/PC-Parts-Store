47d48956033e428bd219efd12cff84b1a7c4c70a
Sajid-Mahir <sajid.imam.mahir@g.bracu.ac.bd>
2023-07-06 22:55:04 +0600

    Featured Products, Description

---
+++ backend/data.js
@@ -0,0 +1,42 @@
+const data = {
+  products: [
+    {
+      name: 'ASUS TUF Gaming GeForce GTX 1660 Ti EVO TOP Edition 6GB GDDR6 Graphics Card',
+      slug: 'ASUS GTX 1660 Ti Graphics Card',
+      category: 'Graphics Card',
+      image: '/images/p1.jpg',
+      price: 450,
+      countInStock: 10,
+      brand: 'Asus',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High Quality Products',
+    },
+    {
+      name: 'AMD Ryzen 5 5600G Processor with Radeon Graphics (Chinese Edition)',
+      slug: 'AMD Ryzen 5 5600G',
+      category: 'Processor',
+      image: '/images/p2.jpg',
+      price: 122,
+      countInStock: 7,
+      brand: 'AMD',
+      rating: 4.8,
+      numReviews: 19,
+      description: 'High Quality Products',
+    },
+    {
+      name: 'Samsung 970 EVO Plus 1TB NVMe M.2 SSD',
+      slug: 'Samsung 970 1TB NVMe',
+      category: 'SSD',
+      image: '/images/p3.jpg',
+      price: 90,
+      countInStock: 12,
+      brand: 'Samsung',
+      rating: 4.7,
+      numReviews: 11,
+      description: 'High Quality Products',
+    },
+  ],
+};
+
+export default data;
---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomeScreen from './Views/HomeScreen';
+import ProductScreen from './Views/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>PC Parts</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container className="mt-3">
+            <Routes>
+              <Route path="/products/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">My rights</div>
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
